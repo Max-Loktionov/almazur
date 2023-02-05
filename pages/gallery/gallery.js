989ed296974c97450a12d401a473843cf90c09d9
max-loktionov <studreno@gmail.com>
2023-02-05 13:02:34 +0200

    add tag video

--- pages/gallery/gallery.js
+++ pages/gallery/gallery.js
@@ -1,8 +1,11 @@
+import Player from "../../components/VideoPlayer/player";
+
 const Gallery = () => {
   return (
     <>
       <h1>Галерея</h1>
       <p>Cards фото та відео</p>
+      <Player source="http://media.w3.org/2010/05/sintel/trailer.mp4" />
     </>
   );
 };
