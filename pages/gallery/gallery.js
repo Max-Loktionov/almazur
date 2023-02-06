e554f5e9da9ea44ce726d0629eb610c1c36550de
max-loktionov <studreno@gmail.com>
2023-02-06 23:22:05 +0200

    add reactplayer

--- pages/gallery/gallery.js
+++ pages/gallery/gallery.js
@@ -1,11 +1,13 @@
 import Player from "../../components/VideoPlayer/player";
+import ReactPlayer from "react-player/youtube";
 
 const Gallery = () => {
   return (
     <>
       <h1>Галерея</h1>
       <p>Cards фото та відео</p>
-      <Player source="http://media.w3.org/2010/05/sintel/trailer.mp4" />
+      {/* <Player source="http://media.w3.org/2010/05/sintel/trailer.mp4" /> */}
+      <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
     </>
   );
 };
